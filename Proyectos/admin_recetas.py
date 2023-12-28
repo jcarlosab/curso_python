19b4b9daf3cb6830fa941e3c0a4e7c79d074878e
JCarlosDev <josecarlos.dev@outlook.es>
2023-12-28 02:56:03 +0100

    Update admin_recetas.py versión base

--- Proyectos/admin_recetas.py
+++ Proyectos/admin_recetas.py
@@ -10,16 +10,19 @@ import os
 from pathlib import Path
 from os import system
 
+ruta = Path(os.getcwd(), "Recetas")
 
-def inicio():
-    ruta = Path(os.getcwd(), "Recetas")
-    print("-" * 45)
-    print("Hola, bienvenido a tu recetario.")
-    print(f"La ruta actual del recetarío es: {ruta}")
-    print(f"El número actual de recetas es: {total_recetas(ruta)}")
-    print("-" * 45)
+
+def inicio(ruta):
     opcion = 'x'
-    while not opcion.isnumeric() or int(opcion) not in range(1, 7) and opcion != 6:
+
+    while not opcion.isnumeric() or int(opcion) not in range(1, 7):
+        limpiar_consola()
+        print("-" * 120)
+        print("\t Hola, bienvenido a tu recetario.")
+        print(f"\t La ruta actual del recetarío es: {ruta}")
+        print(f"\t El número actual de recetas es: {total_recetas(ruta)}")
+        print("-" * 120)
         print("""
                 ------------
                 | Opciones |
@@ -32,65 +35,61 @@ def inicio():
                 [6] Cerrar
             """)
         opcion = input("Selecciona una opción numérica de 1 - 6: ")
-        # limpiar_consola()
-    if opcion == '1':
-        limpiar_consola()
-        leer_receta(ruta)
-        inicio()
-    elif opcion == '2':
-        limpiar_consola()
-        crear_receta(ruta)
-        inicio()
-    elif opcion == '3':
-        limpiar_consola()
-        crear_categoria(ruta)
-        inicio()
-    elif opcion == '4':
-        limpiar_consola()
-        eliminar_receta(ruta)
-        inicio()
-    elif opcion == '5':
-        limpiar_consola()
-        eliminar_categoria(ruta)
-        inicio()
-    elif opcion == '6':
-        limpiar_consola()
-        print("Cerrando programa... ")
+
+    return opcion
 
 
 def listar_categorias(ruta):
     opcion = 'x'
     categorias = os.listdir(ruta)
+
     while not opcion.isnumeric() or int(opcion) not in range(1, len(categorias) + 1):
+        limpiar_consola()
         contador = 1
         for categoria in categorias:
             print(f"[{contador}] - {categoria}")
             contador += 1
         opcion = input("Selecciona el número de categoría: ")
+
     return categorias[(int(opcion) - 1)]
 
 
 def crear_categoria(ruta):
-    ruta = Path(ruta, input("Introduce un nombre de catagoría: "))
-    os.makedirs(ruta)
+    existe = False
+
+    while not existe:
+        limpiar_consola()
+        nombre_categoria = input("Introduce un nombre de catagoría: ")
+        ruta_nueva = Path(ruta, nombre_categoria)
+        if not os.path.exists(ruta_nueva):
+            Path.mkdir(ruta_nueva)
+            input(f"Tu nueva categoria {nombre_categoria} ha sido creada ...presiona enter para volver a inicio")
+            existe = True
+        else:
+            input("Lo siento, esa categoria ya existe ...presiona enter para volver")
+
 
 
 def eliminar_categoria(ruta):
     print("Eliminar categoría\n")
     ruta = Path(ruta, listar_categorias(ruta))
-    os.rmdir(ruta)
+    Path.rmdir(ruta)
 
 
 def listar_recetas(ruta, categoria):
     opcion = 'x'
     directorio = Path(ruta, categoria)
+
     while not opcion.isnumeric() or int(opcion) not in range(1, total_recetas(ruta)):
+        limpiar_consola()
         contador = 1
         for archivo in directorio.glob("**/*.txt"):
             print(f"[{contador}] {archivo.stem}")
             contador += 1
         opcion = input("Indica el número de receta: ")
+
     ruta = Path(ruta, categoria, os.listdir(directorio)[int(opcion) - 1])
+
     return ruta
 
 
@@ -98,29 +97,61 @@ def leer_receta(ruta):
     ruta_receta = Path(listar_recetas(ruta, listar_categorias(ruta)))
     archivo = open(ruta_receta)
     print(archivo.read())  # Pinta el contenido del fichero
+    input("\n Presiona enter para volver a inicio")
     archivo.close()
 
 
 def crear_receta(ruta):
-    categoria = listar_categorias(ruta)
-    nueva_receta = input("Nombre de la receta a crear: ") + '.txt'
-    Path(ruta, categoria)
-    open(nueva_receta, 'x')
-    listar_categorias()
+    existe = False
+
+    while not existe:
+        limpiar_consola()
+        categoria = listar_categorias(ruta)
+        nombre_receta = input("Nombre de la receta: ") + '.txt'
+        ruta_nueva = Path(ruta, categoria, nombre_receta)
+        if not os.path.exists(ruta_nueva):
+            print("Escribe tu nueva receta: ")
+            contenido_receta = input()
+            Path.write_text(ruta_nueva, contenido_receta)
+            input(f"Tu receta {nombre_receta} ha sido creada ...presiona enter para volver a inicio")
+            existe = True
+        else:
+            input("Lo siento, esa receta ya existe ...presiona enter para volver")
 
 
 def eliminar_receta(ruta):
-    os.remove(Path(listar_recetas(ruta, listar_categorias(ruta))))
-
+    receta_eliminada = Path(listar_recetas(ruta, listar_categorias(ruta)))
+    input(f"Eliminada receta {receta_eliminada.name} ...presiona enter para volver a inicio")
+    receta_eliminada.unlink()
 
 def total_recetas(ruta):
     cantidad = 0
+
     for path in Path(ruta).glob("**/*.txt"):
         cantidad += 1
+
     return cantidad
 
 
 def limpiar_consola():
     return system("cls" if os.name in ("nt", "dos") else "clear")
 
-inicio()
+
+finalizar_programa = False
+
+while not finalizar_programa:
+    opcion_menu = inicio(ruta)
+
+    if opcion_menu == '1':
+        leer_receta(ruta)
+    elif opcion_menu == '2':
+        crear_receta(ruta)
+    elif opcion_menu == '3':
+        crear_categoria(ruta)
+    elif opcion_menu == '4':
+        eliminar_receta(ruta)
+    elif opcion_menu == '5':
+        eliminar_categoria(ruta)
+    elif opcion_menu == '6':
+        finalizar_programa = True
+        print("Cerrando programa... ")
